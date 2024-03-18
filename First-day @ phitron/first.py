b631eecc229d18dc682179d4b343275bfc8fcec5
Khaled Karim <kskarim177@gmail.com>
2024-03-18 15:49:05 +0600

    skipping basics of python as I have done it a longg ago

---
+++ First-day @ phitron/first.py
@@ -0,0 +1,14 @@
+print("Hurrayyy")
+
+
+age = 99
+reason = "huddai"
+OOP = "previously done with CSE111 @ BracU"
+print(OOP)
+print(isinstance(OOP,str))
+
+inp = input("Money money money: ")
+print(inp)
+x = 5
+y = 3
+print(x % y)
