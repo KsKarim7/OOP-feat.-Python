2890613aa6c6609a4a1757467e1ed33eb7667358
Khaled Karim <kskarim177@gmail.com>
2024-03-20 22:59:00 +0600

    submitted

--- Problem Solving/Mod 3.5/replace_word.py
+++ Problem Solving/Mod 3.5/replace_word.py
@@ -1,18 +1,18 @@
-# s = input()
-s = "BRITISHEGYPTGHANA"
+s = input()
+# s = "ITALYKOREAEGYPTEGYPTALGERIAEGYPTZ"
 # lst = s.split("EGYPT")
 # print(s[2:7])
 k = 0
-ss = " "
+ss = ""
 for i in range(len(s)):
     if(k+5 < len(s)):
         if(s[k:k+5] == "EGYPT"):
-            k+=6
+            k+=5
             ss+=" "
         else:
             ss += s[k]
             k+=1
-    if(k<len(s)):  
+    elif(k<len(s)):  
             ss += s[k]
             k+=1
 print(ss)
