bbcfa78373fb9747282447f3bb648a6cf8b80c32
Khaled Karim <kskarim177@gmail.com>
2024-03-23 17:51:35 +0600

    abstract methoc

---
+++ OOP-Basic/inheritance_ concpets.py
@@ -0,0 +1,15 @@
+# base class, parent class
+
+class BaseClass:
+    pass
+
+
+# Derived class or child class 
+class DerivedClass(BaseClass):
+    pass
+
+# 1) Simple Inheritance: parent class -> clild class 
+# 2) Multi level inheritance : GrandPapa -> Papa -> Child
+# 3. Multiple inheritance: Student (Family, School, Sports)
+
+# 4. Hybrid: Granda --> Father, Uncle, Aunty --> Child (Father, Uncle)
